5c7981f7330d6a99b73d47e83160e41f9d56dc61
Francisco Fernandez Guerra <105071395+fmfg03@users.noreply.github.com>
2025-04-19 20:08:32 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      // Include .js files as JSX
+      include: "**/*.{jsx,js}",
+    })
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
