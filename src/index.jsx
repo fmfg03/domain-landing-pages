1bbb4980dc36df57c689e3407060956b9f9992eb
Francisco Fernandez Guerra <105071395+fmfg03@users.noreply.github.com>
2025-04-19 20:14:34 -0600

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import DomainRouter from './components/DomainRouter';
+import DomainRouter from './components/DomainRouter.jsx';
+import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
